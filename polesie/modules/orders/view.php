557a529cec73cbdd20dd6354cea50a7f614731ba
Артем <lemeshtema@mail.ru>
2026-06-13 20:17:35 +0300

    update text button

--- polesie/modules/orders/view.php
+++ polesie/modules/orders/view.php
@@ -479,7 +479,7 @@ $pageTitle = 'Заказ №' . e($order['order_number']);
                         <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right: 6px;">
                             <path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
-                        Перейти в производство
+                        Перейти к производству
                     </a>
                     <a href="edit.php?id=<?= $order['id'] ?>" class="btn btn-primary">
                         <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right: 6px;">
